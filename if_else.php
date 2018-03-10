d5f7ea53bd7bb6768aece273109b5a6d040d0058
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-11 01:26:06 +0600

    control structure: if else

---
+++ if_else.php
@@ -0,0 +1,16 @@
+<?php
+
+    $first_name = "Mehedi";
+    $official_name = "Mehedi";
+
+    if ($first_name == $official_name) {
+
+        echo "both variables are same.";
+    }
+
+    else {
+
+        echo "both variables are not same.";
+    }
+
+?>
