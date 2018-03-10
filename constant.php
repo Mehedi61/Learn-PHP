f9571fca6778b9fadb96536bfe8ebb33dff6c18f
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-11 00:44:44 +0600

    define and print constant

---
+++ constant.php
@@ -0,0 +1,11 @@
+<?php
+
+    // define constant
+    define("ENGINEERS", " can solve problems.");
+    define("DOCTORS", " can help us to get well.");
+
+    // print constant
+    echo "Engineers" . ENGINEERS ."<br>"; 
+    echo "Doctors" . DOCTORS;
+
+?>
